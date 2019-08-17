1e262f76942ea679e25c5e07e2b18be45c870923
cdra0624 <t.adaniya@adawarp.com>
2019-08-17 21:14:26 +0900

    feat: add change-image sample

---
+++ html-js/change-image/main.js
@@ -0,0 +1,4 @@
+"use strict";
+
+//wrtie code in here
+
--- html-js/change-text/main.js
+++ html-js/change-text/main.js
@@ -1,3 +1,29 @@
 "use strict"
 
-// write code in here
+const textElement = document.getElementById("text")
+const appleButton = document.getElementById("apple")
+const bananaButton = document.getElementById("banana")
+const orangeButton = document.getElementById("orange")
+const resetButton = document.getElementById("RESET")
+
+
+function appleClick() {
+  textElement.innerHTML = "APPLE"
+}
+
+function bananaClick() {
+  textElement.innerHTML = "BANANA"
+}
+
+function orangeClick() {
+  textElement.innerHTML = "ORANGE"
+}
+
+function resetClick() {
+  textElement.innerHTML = "-"
+}
+
+appleButton.addEventListener("click", appleClick)
+bananaButton.addEventListener("click", bananaClick)
+orangeButton.addEventListener("click", orangeClick)
+resetButton.addEventListener("click", resetClick)
