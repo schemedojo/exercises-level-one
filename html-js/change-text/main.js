277512bd0024c8482a17ed96dd3d6d127e357e89
cdra0624 <t.adaniya@adawarp.com>
2019-08-18 14:13:59 +0900

    remove js script

--- html-js/change-text/main.js
+++ html-js/change-text/main.js
@@ -1,29 +1,3 @@
 "use strict"
 
-const textElement = document.getElementById("text")
-const appleButton = document.getElementById("apple")
-const bananaButton = document.getElementById("banana")
-const orangeButton = document.getElementById("orange")
-const resetButton = document.getElementById("RESET")
-
-
-function appleClick() {
-  textElement.innerHTML = "APPLE"
-}
-
-function bananaClick() {
-  textElement.innerHTML = "BANANA"
-}
-
-function orangeClick() {
-  textElement.innerHTML = "ORANGE"
-}
-
-function resetClick() {
-  textElement.innerHTML = "-"
-}
-
-appleButton.addEventListener("click", appleClick)
-bananaButton.addEventListener("click", bananaClick)
-orangeButton.addEventListener("click", orangeClick)
-resetButton.addEventListener("click", resetClick)
+// write code in here
